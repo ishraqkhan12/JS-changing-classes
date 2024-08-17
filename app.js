e6d9d01aea1b5fdaacce2632863616f0cedb4085
ishraqkhan12 <157279448+ishraqkhan12@users.noreply.github.com>
2024-08-17 15:55:42 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function swapPic(id, newImg){
+    document.getElementById(id).src = newImg
+}
+
+let a = document.getElementById("id1")
+a.className = "para"
+
+
+let img = document.getElementById("image")
+ img.src = "https://c4.wallpaperflare.com/wallpaper/902/510/444/flowers-nature-background-wallpaper-preview.jpg"
